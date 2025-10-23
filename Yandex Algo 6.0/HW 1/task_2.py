b4cf68578a53eacf09e64a213637975b87d45a31
Nuradil Serik <serik.nuradil@gmail.com>
2025-10-23 15:58:54 +0500

    Start of 2nd task of HW 1

---
+++ Yandex Algo 6.0/HW 1/task_2.py
@@ -0,0 +1,9 @@
+def min_takes(A, B, C, D):
+    print(f"Number of blue shirts {A}")
+    print(f"Number of red shirts {B}")
+    print(f"Number of blue socks {C}")
+    print(f"Number of red socks {D}")
+    
+    
+    
+    
